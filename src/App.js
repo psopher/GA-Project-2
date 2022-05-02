f6e8e5134a5c049e313ef49298cef5ba8957307d
Philip Sopher <phsophe@gmail.com>
2022-05-02 15:19:36 +0200

    full starter project 2 starter file

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+// Import Components
+import Home from './components/Home'
+import PageNavBar from './components/common/PageNavBar'
+import NotFound from './components/common/NotFound'
+
+
+
+const App = () => {
+  return (
+    <h1>Project 2 Start</h1>
+  )
+}
+
+export default App
---
+++ src/components/common/PageNavBar.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import { Link, useNavigate } from 'react-router-dom'
+
+// Import React Components
+import Navbar from 'react-bootstrap/Navbar'
+import Container from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap/Nav'
+
+// Import helpers
+import { userIsAuthenticated } from '../../helpers/auth'
+
+const PageNavBar = () => {
+
+  return (
+    <>
+    </>
+  )
+
+}
+
+export default PageNavBar
